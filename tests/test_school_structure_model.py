32e1bf79ff6e839e530541aac3c7c40bcb1e89eb
bllli <me@lidengming.com>
2017-05-17 15:13:02 +0800

    单元测试: 学校结构model (感觉...不值得特地测试一遍...)

---
+++ tests/test_school_structure_model.py
@@ -0,0 +1,57 @@
+import unittest
+from app import create_app, db
+from app.models import School, Department, Specialty, _Class, User, Role
+
+
+class SchoolStructureModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_department_in_school(self):
+        s = School(name='tsxy')
+        db.session.add(s)
+        db.session.commit()
+        d = Department(name='cs')
+        d.school = s
+        db.session.add(d)
+        db.session.commit()
+        self.assertTrue(d in s.departments)
+
+    def test_specialty_in_department(self):
+        d = Department(name='cs')
+        db.session.add(d)
+        db.session.commit()
+        s = Specialty(name='cs')
+        s.department = d
+        db.session.add(s)
+        db.session.commit()
+        self.assertTrue(s in d.specialties)
+
+    def test_class_in_specialty(self):
+        s = Specialty(name='cs')
+        db.session.add(s)
+        db.session.commit()
+        c = _Class(name='14CS1')
+        c.specialty = s
+        db.session.add(c)
+        db.session.commit()
+        self.assertTrue(c in s.classes)
+
+    def test_student_in_class(self):
+        c = _Class(name='14CS1')
+        db.session.add(c)
+        db.session.commit()
+        u = User(name='bllli')
+        u._class = c
+        db.session.add(c)
+        db.session.commit()
+        self.assertTrue(u in c.students)
