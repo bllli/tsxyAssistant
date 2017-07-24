491c5ee8fc7dadb8c299c56cc10411b169c52d21
bllli <me@lidengming.com>
2017-07-25 00:39:14 +0800

    + web端 base页/主页 (作者:单若宁)

---
+++ app/static/js/animate.js
@@ -0,0 +1,24 @@
+$(function() {
+	//导航条切换
+	$(".top ul li").each(function(index) {
+		$(this).click(function() {
+			$("li").removeClass("active");
+			$("li").eq(index).addClass("active");
+		});
+	});
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+})
