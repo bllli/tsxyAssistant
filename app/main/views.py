94e00a242042af57b8f17f131c92310f603b589b
bllli <me@lidengming.com>
2017-08-15 22:21:55 +0800

    - 前后交互改用mvvm 去除项目中无用的前端代码及文件

--- app/main/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-from flask import render_template
-from . import main
-
-
-@main.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
